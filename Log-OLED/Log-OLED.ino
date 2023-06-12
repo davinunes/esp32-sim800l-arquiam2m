4cd629e344ee515b7ef9203777a9bcf94a7c631b
Davi Nunes <davinunes.franca@eb.mil.br>
2023-06-12 15:17:28 -0300

    update oled

--- Log-OLED/Log-OLED.ino
+++ Log-OLED/Log-OLED.ino
@@ -10,8 +10,18 @@ int tail = 0;
 
 int contador = 0;
 
+#include <HardwareSerial.h>
+
+// Configuração dos pinos do SIM800L
+#define SIM800_TX_PIN 16
+#define SIM800_RX_PIN 17
+
+HardwareSerial sim800l(1);
+
 void setup() {
   Serial.begin(115200);
+
+  sim800l.begin(9600, SERIAL_8N1, SIM800_RX_PIN, SIM800_TX_PIN);
 
   // INICIALIZA O DISPLAY & INVERTE O DISPLAY VERTICALMENTE
   display.init();
@@ -20,8 +30,21 @@ void setup() {
 
 void loop() {
   incrementarContador();
-  adicionarValorAoBuffer(String(contador));
+  //adicionarValorAoBuffer(String(contador));//
   atualizarTela();
+
+  int sinal = obterSinal();
+  bool statusGPRS = obterStatusGPRS();
+  String imei = obterIMEI();
+  bool simCard = verificarSIMCard();
+
+  adicionarValorAoBuffer("" + imei);
+  adicionarValorAoBuffer("Sinal: " + String(sinal));
+  adicionarValorAoBuffer("GPRS: " + String(statusGPRS));
+  adicionarValorAoBuffer("SIM: " + String(simCard));
+  Serial.println("volta");
+
+
 
   delay(1000);
 }
@@ -42,9 +65,10 @@ void adicionarValorAoBuffer(String valor) {
 }
 
 void atualizarTela() {
+
   display.clear();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_16);
+  display.setFont(ArialMT_Plain_10);
 
   int linha = 0;
   int index = tail;
@@ -55,4 +79,67 @@ void atualizarTela() {
   }
 
   display.display();
+}
+
+int obterSinal() {
+  sim800l.println("AT+CSQ");
+  delay(1000);
+  while (sim800l.available()) {
+    String response = sim800l.readStringUntil('\n');
+    if (response.startsWith("+CSQ:")) {
+      int commaIndex = response.indexOf(",");
+      if (commaIndex != -1) {
+        int signalValue = response.substring(commaIndex + 1).toInt();
+        return signalValue;
+      }
+    }
+  }
+  return 0; // Retornar 0 se não for possível obter o valor do sinal
+}
+
+bool obterStatusGPRS() {
+  sim800l.println("AT+CGATT?");
+  delay(1000);
+  while (sim800l.available()) {
+    String response = sim800l.readStringUntil('\n');
+    if (response.startsWith("+CGATT:")) {
+      int commaIndex = response.indexOf(":");
+      if (commaIndex != -1) {
+        int status = response.substring(commaIndex + 1).toInt();
+        return (status == 1);
+      }
+    }
+  }
+  return false; // Retornar false se não for possível obter o status GPRS
+}
+
+String obterIMEI() {
+  sim800l.println("AT+GSN");
+  delay(1000);
+  while (sim800l.available()) {
+    String response = sim800l.readStringUntil('\n');
+    response.trim(); // Remover espaços em branco no início e no final da resposta
+    if (response.length() == 15) {
+      return response;
+    }
+  }
+  return "Desconhecido"; // Retornar "Desconhecido" se não for possível obter o IMEI
+}
+
+bool verificarSIMCard() {
+  sim800l.println("AT+CPIN?");
+  delay(1000);
+  while (sim800l.available()) {
+    String response = sim800l.readStringUntil('\n');
+    response.trim(); // Remover espaços em branco no início e no final da resposta
+    if (response.startsWith("+CPIN:")) {
+      int colonIndex = response.indexOf(":");
+      if (colonIndex != -1) {
+        String status = response.substring(colonIndex + 1);
+        status.trim();
+        return (status == "READY");
+      }
+    }
+  }
+  return false; // Retornar false se não for possível obter o status do SIM card
 }
